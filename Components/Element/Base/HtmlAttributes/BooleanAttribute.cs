ca2994e19cbe91998c52c6811bc0bf7dce411408
Dennis Mollet <Dennis.Mollet@Gmail.com>
2019-04-03 21:18:03 -0700

    More work on Html Elements and Attributes

---
+++ Components/Element/Base/HtmlAttributes/BooleanAttribute.cs
@@ -0,0 +1,19 @@
+namespace Blazor.HtmlElements
+{
+    public class BooleanAttribute : HtmlAttribute
+    {
+        public BooleanAttribute(string attribute)
+            : base()
+        {
+           _Key = attribute;
+        }
+
+        protected string _Key {get;set;}
+        public override string Key => _Key;
+
+        public override string BuildAttribute()
+        {
+            return $"{Key}";
+        }
+    }
+}
